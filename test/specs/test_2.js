b9ecd435800c4098e54aa3fc9c082f4766f62f48
Mrugesh666 <mrugeshbhatia666@gmail.com>
2026-03-18 00:05:56 +0530

    change_id

--- test/specs/test_2.js
+++ test/specs/test_2.js
@@ -4,7 +4,7 @@ describe('LoginPage WITH POM', () => {
 
     it('Login', async () => {
         browser.url('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        await loginPage.login("Admin", "admin123")
+        await loginPage.login("Martin", "admin123")
         })
 })
 
